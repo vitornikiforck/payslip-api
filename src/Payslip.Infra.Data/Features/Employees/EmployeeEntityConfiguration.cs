42087a1843489002b5fc0c625f51232edf7894bc
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-15 21:00:36 -0300

    added context, repository and entity configuration

---
+++ src/Payslip.Infra.Data/Features/Employees/EmployeeEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Payslip.Domain.Features.Employees;
+
+namespace Payslip.Infra.Data.Features.Employees
+{
+    internal class EmployeeEntityConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(e => e.LastName).HasMaxLength(100).IsRequired();
+            builder.Property(e => e.Department).HasMaxLength(30).IsRequired();
+            builder.Property(e => e.GrossSalary).HasPrecision(6, 2).IsRequired();
+            builder.Property(e => e.AdmissionDate).IsRequired();
+            builder.Property(e => e.HealthPlan).IsRequired();
+            builder.Property(e => e.DentalPlan).IsRequired();
+            builder.Property(e => e.TransportantionVoucher).IsRequired();
+            builder.Property(e => e.UpdateAt).IsRequired();
+            builder.Property(e => e.IsRemoved).IsRequired();
+
+            builder.HasQueryFilter(e => !e.IsRemoved);
+        }
+    }
+}
