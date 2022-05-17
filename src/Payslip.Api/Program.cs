9dc22e53fe9fbc909e7af212cca5800ad90727a9
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-17 20:03:58 -0300

    added new startup and appsettings

--- src/Payslip.Api/Program.cs
+++ src/Payslip.Api/Program.cs
@@ -1,50 +1,5 @@
-using Microsoft.OpenApi.Models;
+using Payslip.Api;
 using Payslip.Api.Extensions;
-using Payslip.Infra.Data.Contexts;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddAutoMapper();
-builder.Services.AddCors();
-builder.Services.AddHealthCheckSMiddleware<PayslipDbContext>();
-builder.Services.AddDependecies(builder.Configuration);
-
-builder.Services.AddMVC();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen(c =>
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Payslip.Api", Version = "v1" })
-);
-
-//MediatorExtensions.AddMediator();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-HealthChecksExtensions.UseHealthChecksMiddleware(app);
-
-app.UseHealthChecksMiddleware();
-
-app.UseCorsConfig();
-
-app.ApplyMigrations();
-
-app.Run();
+var builder = WebApplication.CreateBuilder(args)
+.UseStartup<Startup>();
---
+++ src/Payslip.Api/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Autofac;
+using Microsoft.OpenApi.Models;
+using Payslip.Api.Extensions;
+using Payslip.Infra.Data.Contexts;
+
+namespace Payslip.Api
+{
+    public class Startup : IStartup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Payslip.Api", Version = "v1" })
+            );
+
+            services.AddAutoMapper();
+            services.AddCors();
+            services.AddHealthChecksMiddleware<PayslipDbContext>();
+            services.AddDependecies(Configuration);
+            services.AddMVC();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCorsConfig();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.UseHealthChecksMiddleware();
+
+            app.ApplyMigrations();
+        }
+    }
+}
